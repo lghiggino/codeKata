484c2638a6b76413b6608a328ae77846bb0c6bcb
Leonardo Ghiggino <lghiggino@gmail.com>
2024-05-07 09:57:37 -0300

    convert pound to ounces - rounding amount

--- src/kata01/kata01.spec.ts
+++ src/kata01/kata01.spec.ts
@@ -1,8 +1,6 @@
-import { concat } from '../helpers'
-
 describe('kata 01', () => {
   it('should calculate the value for bundle itens in the N for X dollar model', () => {
-    function getProdutPromo(productId: string) {
+    function getProductPromo(productId: string) {
         if (productId === 'banana') {
           return {
             bundleValue: 100,
@@ -20,7 +18,7 @@ describe('kata 01', () => {
     }
 
     function calculateBundleValue(amount: number, productId: string): number {
-      const { bundleValue, individualValue, breakAmount } = getProdutPromo(productId)
+      const { bundleValue, individualValue, breakAmount } = getProductPromo(productId)
 
       const rest = amount % breakAmount
       const bundles = Math.floor(amount / breakAmount)
@@ -33,5 +31,22 @@ describe('kata 01', () => {
     expect(calculateBundleValue(9, 'banana')).toBe(300)
     expect(calculateBundleValue(10, 'apple')).toBe(400)
     expect(calculateBundleValue(12, 'apple')).toBe(420)
+  })
+
+  it('should calculate pound to ounces convertions: $1.99/pound (so what does 4 ounces cost?)', () => {
+    function convertPoundToOunces(poundValue: number): number {
+      return poundValue / 16
+    }
+
+    expect(convertPoundToOunces(1.99)).toBe(0.124375)
+  })
+
+  it('should calculate pound to ounces convertions: should it round the final amount?', () => {
+    function convertPoundToOunces(poundValue: number): number {
+      return +(poundValue / 16).toFixed(2)
+    }
+
+    expect(convertPoundToOunces(1.99)).toBe(0.12)
+    expect(convertPoundToOunces(2.01)).toBe(0.13)
   })
 })
